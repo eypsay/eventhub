02fca762c63dc3c9f3677ec410de44ab700ba160
eyp <eypsay@yahoo.com>
2026-09-12 19:45:13 +0300

    exception handler adding

--- src/main/java/com/eventhub/api/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/eventhub/api/Exception/GlobalExceptionHandler.java
@@ -1,9 +1,36 @@
 package com.eventhub.api.Exception;
 
+import com.eventhub.api.dto.ApiErrorResponse;
+import com.eventhub.api.dto.FieldErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationException(MethodArgumentNotValidException exception,
+                                                                      HttpServletRequest request) {
+        List<FieldErrorResponse> fieldErrors = exception.getFieldErrors()
+                .stream()
+                .map(e -> new FieldErrorResponse(e.getField(), e.getCode(), e.getDefaultMessage()))
+                .toList();
+        String path = request.getRequestURI();
+        Instant timestamp = Instant.now();
+        int status = HttpStatus.BAD_REQUEST.value();
+        String code = "VALIDATION_ERROR";
+        String message = "Request validation failed";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ApiErrorResponse(timestamp, status, code, message, path, fieldErrors)
+        );
+    }
 
 }
